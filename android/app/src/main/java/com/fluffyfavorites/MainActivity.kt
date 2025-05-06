bdcf0c0710c0c6181a1ea5f2476c278418a74802
Veronika <veronikapetrushka@gmail.com>
2025-05-06 09:02:48 +0200

    android configs

--- android/app/src/main/java/com/fluffyfavorites/MainActivity.kt
+++ android/app/src/main/java/com/fluffyfavorites/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "FluffyFavorites"
+  override fun getMainComponentName(): String = "Fluffy Pet Care"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
